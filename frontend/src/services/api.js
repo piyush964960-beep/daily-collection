4a6c345b6bbb651bee70c1674c2c61e810322486
unknown <piyush.gupta@aviso.com>
2026-04-19 16:07:00 +0530

    Added base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://daily-collection-vdxo.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 })
 
